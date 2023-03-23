cefb2fc3d1e708bb47a9265b602bc57981157945
NathanTrance <116446921+NathanTrance@users.noreply.github.com>
2023-03-23 13:11:13 +0700

    Update Preprocessing1.py

--- Preprocessing1.py
+++ Preprocessing1.py
@@ -3,8 +3,8 @@ import wfdb
 import pandas as pd
 import ast
 import cv2
-import skimage
-import cohen as ch
+# import skimage
+# import cohen as ch
 # import tftb
 import scipy as scp
 from scipy import signal as sg
@@ -12,194 +12,125 @@ import io
 # import matplotlib.pyplot as plt
 # import matplotlib.cm as cm
 import pywt
-
-path = "/home/ubuntu/Tue.CM210908/data/physionet.org/files/ptb-xl/1.0.3/"
-sampling_rate = 100
-cm_jet = cm.get_cmap('jet')
-
-class Preprocessing():
-    def __init__(self,path,sampling_rate,experiment = 'diagnostic_superclass'):
-        self.experiment = experiment
-        self.path = path
-        self.sampling_rate = sampling_rate
-        self.csv_file = pd.read_csv(path+'ptbxl_database.csv',index_col = 'ecg_id')
-        self.csv_file.scp_codes = self.csv_file.scp_codes.apply(lambda x: ast.literal_eval(x))
-        
-        self.num_of_class = 5
-        if (self.experiment == 'diagnostic_subclass'):
-            self.num_of_class = 23
-
-        self.agg_df = pd.read_csv(path+'scp_statements.csv', index_col=0)
-        self.agg_df = self.agg_df[self.agg_df.diagnostic == 1]
-        # Apply diagnostic superclass
-        self.csv_file['diagnostic_superclass'] = self.csv_file.scp_codes.apply(self.aggregate_diagnostic)
-        self.csv_file['diagnostic_subclass'] = self.csv_file.scp_codes.apply(self.aggregate_subdiagnostic)
-        # Split data into train and test
-        self.test_fold = 10
-        self.val_fold = 9
-
-        #soft_label_encoding
-        self.meta_sle = np.full((21799,32),0.1)
-        self.meta_sle[self.csv_file.sex==1,0] = 1
-        self.meta_sle[self.csv_file.sex==0,1] = 1
-        for i in range(10):
-            self.meta_sle[np.logical_and(np.array(self.csv_file.height)>=(100+10*i), np.array(self.csv_file.height)<(110+10*i)), 2+i] = 1 #height encoding
-            self.meta_sle[np.logical_and(np.array(self.csv_file.weight)>=(0+12*i), np.array(self.csv_file.weight)<(12+12*i)), 12+i] = 1 #weight encoding
-            self.meta_sle[np.logical_and(np.array(self.csv_file.age)>=(0+10*i), np.array(self.csv_file.age)<(10+10*i)), 22+i] = 1 #age encoding
-        self.meta_sle[self.csv_file.age == 300, 31] = 1
-
-        #uncomment the following block for SLE
-
-        def check_not_empty(metadata):
-            bool1 = 1 in metadata[2:11]
-            bool2 = 1 in metadata[12:21]
-            return [bool1, bool2]
-
-        def fill_noise(metadata):
-            bool = check_not_empty(metadata)
-            male_height =   [0,7,7,7,7,7,7,7,7,7]
-            female_height = [1,6,6,6,6,6,6,6,6,5]
-            male_weight =   [1,6,6,6,6,6,6,6,5,5]
-            female_weight = [1,5,5,5,5,5,5,5,4,4]
-            if not bool[0]:
-                if metadata[0] == 1:
-                    l = female_height
-                else: l = male_height
-                for i in range(22,32):
-                    if metadata[i] == 1:
-                        break
-                metadata[l[i-22]+2] = 1
-            if not bool[1]:
-                if metadata[0] == 1:
-                    l2 = female_weight
-                else: l2 = male_weight
-                for j in range(22,32): #age indicator
-                    if metadata[j] == 1:
-                        break
-                metadata[l2[j-22]+12] = 1
-            return metadata        
-
-        for i in range(21799):
-            self.meta_sle[i] = fill_noise(self.meta_sle[i])
-
-        #one hot coding for y
-        self.y = np.zeros((21799,5))
-        if (self.experiment == "diagnostic_superclass"):
-            l1 = ['NORM' in i for i in self.csv_file.diagnostic_superclass]
-            l2 = ['MI' in i for i in self.csv_file.diagnostic_superclass]
-            l3 = ['HYP' in i for i in self.csv_file.diagnostic_superclass]
-            l4 = ['STTC' in i for i in self.csv_file.diagnostic_superclass]
-            l5 = ['CD' in i for i in self.csv_file.diagnostic_superclass]
-            self.y[l1,0] = 1
-            self.y[l2,1] = 1
-            self.y[l3,2] = 1
-            self.y[l4,3] = 1
-            self.y[l5,4] = 1
-        elif (self.experiment == "diagnostic_subclass"):
-            self.y = np.zeros((21799,23))
-            self.l = ['_AVB','AMI','CLBBB','CRBBB','ILBBB','IMI','IRBBB','ISC_','ISCA','ISCI','IVCD','LAFB/LPFB','LAO/LAE','LMI','LVH','NORM','NST_','PMI','RAO/RAE','RVH','SEHYP','STTC','WPW']
-            for j in range(23):
-                l_j = [self.l[j] in i for i in self.csv_file.diagnostic_subclass]
-                self.y[l_j, j] = 1
-
-        #meta_sle
-        #if using the baseline models, please reshape y_train,y_val and y_test into (-1,self.num_of_class)
-
-        #có thể comment out đống này nếu đã có file X_train.npy
-        X = self.load_raw_data(self.csv_file, self.sampling_rate, self.path)
-        self.X_train = X[np.where(self.csv_file.strat_fold <= 8)]
-        self.X_val = X[np.where(self.csv_file.strat_fold == 9)]
-        self.X_test = X[np.where(self.csv_file.strat_fold == 10)]
-        self.X_train = self.split_reshape(self.X_train)
-        self.X_val = self.split_reshape(self.X_val)
-        self.X_test = self.split_reshape(self.X_test)
-
-        self.y_train = self.y[np.where(self.csv_file.strat_fold <= 8)]
-        self.y_train = np.reshape(self.y_train,(-1,self.num_of_class))
-        self.sle_train = self.meta_sle[np.where(self.csv_file.strat_fold <= 8)]
-        self.sle_train = np.reshape(self.sle_train,(-1,32,1))
-
-        self.y_train, index = self.remove_wrong_labels(self.y_train)
-        self.X_train = self.remove_wrong_labels(self.X_train, index)
-        self.sle_train = self.remove_wrong_labels(self.sle_train,index)
-
-        #extract data       
-        self.y_val = self.y[np.where(self.csv_file.strat_fold == 9)]
-        self.y_val = np.reshape(self.y_val,(-1,self.num_of_class))
-        self.sle_val = self.meta_sle[np.where(self.csv_file.strat_fold == 9)]
-        self.sle_val = np.reshape(self.sle_val,(-1,32,1))
-
-        self.y_val, index = self.remove_wrong_labels(self.y_val)
-        self.X_val = self.remove_wrong_labels(self.X_val, index)
-        self.sle_val = self.remove_wrong_labels(self.sle_val,index)
-
-        self.y_test = self.y[np.where(self.csv_file.strat_fold == 10)]
-        self.y_test = np.reshape(self.y_test,(-1,self.num_of_class))
-        self.sle_test = self.meta_sle[np.where(self.csv_file.strat_fold == 10)]
-        self.sle_test = np.reshape(self.sle_test,(-1,32,1))
-
-        self.y_test, index = self.remove_wrong_labels(self.y_test)
-        self.X_test = self.remove_wrong_labels(self.X_test, index)
-        self.sle_test = self.remove_wrong_labels(self.sle_test,index)
-
-    
-    def load_raw_data(self,df,sampling_rate,path):
-        if (sampling_rate == 100):
-            data = [wfdb.rdsamp(path+ f,channels=[0,1,2,3,4,5,6,7,8,9,10,11]) for f in df.filename_lr]
+from sklearn import preprocessing
+def init_everything():
+    def load_raw_data(df, sampling_rate, path):
+        if sampling_rate == 100:
+            data = [wfdb.rdsamp(path+f,channels=[0,1,2]) for f in df.filename_lr]
         else:
-            data = [wfdb.rdsamp(path+ f,channels=[0,1,2,3,4,5,6,7,8,9,10,11]) for f in df.filename_hr]
+            data = [wfdb.rdsamp(path+f,channels=[0,1,2]) for f in df.filename_hr]
         data = np.array([signal for signal, meta in data])
         return data
 
-    def aggregate_diagnostic(self,y_dic):
+    path = ''
+    sampling_rate=100
+
+    # load and convert annotation data
+    Y = pd.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
+    Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
+
+    # Load raw signal data
+    X = load_raw_data(Y, sampling_rate, path)
+
+    # Load scp_statements.csv for diagnostic aggregation
+    agg_df = pd.read_csv(path+'scp_statements.csv', index_col=0)
+    agg_df = agg_df[agg_df.diagnostic == 1]
+
+    def aggregate_diagnostic(y_dic):
         tmp = []
         for key in y_dic.keys():
-            if key in self.agg_df.index:
-                tmp.append(self.agg_df.loc[key].diagnostic_class)
+            if key in agg_df.index:
+                tmp.append(agg_df.loc[key].diagnostic_class)
         return list(set(tmp))
-
     def aggregate_subdiagnostic(self,y_dic):
         tmp = []
         for key in y_dic.keys():
             if key in self.agg_df.index:
                 tmp.append(self.agg_df.loc[key].diagnostic_subclass)
         return list(set(tmp))
+    val_fold = 9
+    test_fold = 10
 
-    #reshape dataset as model dimention for input
-    def split_reshape(self,input):
-        # join all leads
-        inputs = np.split(input,input.shape[2],axis=2)
-        for i in range(12):
-            inputs[i] = np.reshape(inputs[i],(-1,1,input.shape[1]))
-        inputs = np.concatenate(inputs, axis =1)
-        return inputs
+    # Apply labels
+    experiment = 'diagnostic_superclass'
+    if experiment == 'diagnostic_superclass':
+        Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
+        y_train = Y[np.where(Y.strat_fold <= 8)].diagnostic_superclass
+        y_val = Y[np.where(Y.strat_fold == 9)].diagnostic_superclass
+        y_test = Y[np.where(Y.strat_fold == test_fold)].diagnostic_superclass
+    elif experiment == 'diagnostic_subclass':
+        Y['diagnostic_subclass'] = Y.scp_codes.apply(aggregate_subdiagnostic)
+        y_train = Y[np.where(Y.strat_fold <= 8)].diagnostic_subclass
+        y_val = Y[np.where(Y.strat_fold == 9)].diagnostic_subclass
+        y_test = Y[np.where(Y.strat_fold == test_fold)].diagnostic_subclass
+        
+    X_train = X[np.where(Y.strat_fold <= 8)].transpose(0, 2, 1) 
+    X_val = X[np.where(Y.strat_fold == 9)].transpose(0, 2, 1) 
+    X_test = X[np.where(Y.strat_fold == test_fold)].transpose(0, 2, 1) 
 
-    def remove_wrong_labels(self,y,index = None):
-        if index is None:    
-            check = np.sum(y,axis = 1)
-            index = check == 0
-            return np.delete(y,index,axis = 0), index
-        else:
-            return np.delete(y,index,axis = 0)
+    #label binarizing
+    mlb = preprocessing.MultiLabelBinarizer()
+    if experiment == "diagnostic_superclass":
+        mlb.fit([['CD','HYP','MI','NORM','STTC']])
+    elif experiment == "diagnostic_subclass":
+        mlb.fit([['_AVB','AMI','CLBBB','CRBBB','ILBBB','IMI','IRBBB','ISC_','ISCA','ISCI','IVCD','LAFB/LPFB','LAO/LAE','LMI','LVH','NORM','NST_','PMI','RAO/RAE','RVH','SEHYP','STTC','WPW']])
+    y_train = mlb.transform(y_train)
+    y_val = mlb.transform(y_val)
+    y_test = mlb.transform(y_test)
 
-    def get_data_x(self):
-        return self.X_train,self.X_val,self.X_test
-    
-    def get_fast_data_x(self):
-        return np.load("X_train.npy"),np.load("X_val.npy"),np.load("X_test.npy")
-    
-    def get_data_cwt(self):
-        return np.load("cwt_train.npy"), np.load("cwt_val.npy"), np.load("cwt_test.npy")
-    
-    def get_data_stft(self):
-        return np.load("stft_train.npy"), np.load("stft_val.npy"), np.load("stft_test.npy")
-    
-    def get_data_y(self):
-        return self.y_train,self.y_val,self.y_test
-    
-    def get_data_metadata(self):
-        return self.sle_train,self.sle_val,self.sle_test
+    #remove zeros labels
+    remove_zeros = True
+    if remove_zeros:
+        a = []
+        for y in y_train:
+            if len(y) == 0:
+                a.append(False)
+            else:
+                a.append(True)
+        X_train = X_train[a]
+        y_train = y_train[a]
 
+        a = []
+        for y in y_val:
+            if len(y) == 0:
+                a.append(False)
+            else:
+                a.append(True)
+        X_val = X_val[a]
+        y_val = y_val[a]
+
+        a = []
+        for y in y_test:
+            if len(y) == 0:
+                a.append(False)
+            else:
+                a.append(True)
+        X_test = X_test[a]
+        y_test = y_test[a]
+
+    # Apply bandpass filter
+
+    # saving the data
+    if experiment == "diagnostic_superclass":
+        np.save("X_super_train", x_train)
+        np.save("X_super_val",   x_val)
+        np.save("X_super_test",  x_test)
+        np.save("y_super_train", y_train)
+        np.save("y_super_val",   y_val)
+        np.save("y_super_test",  y_test)
+    elif experiment == "diagnostic_subclass":
+        np.save("X_sub_train", x_train)
+        np.save("X_sub_val",   x_val)
+        np.save("X_sub_test",  x_test)
+        np.save("y_sub_train", y_train)
+        np.save("y_sub_val",   y_val)
+        np.save("y_sub_test",  y_test)
+    
+    return x_train, x_val, x_test
+
+xtrain,xval,xtest = init_everything()
+print(xtrain.shape,xval.shape,xtest.shape)
+#Transformations
 def bandpass(data, lead_index, bp = [3,45], sp = [1,50], gpass = 0.4, spass = 3, fs = sampling_rate, median = False):
     n, wn = scp.signal.buttord(bp, sp, gpass, spass, fs = fs)
     b, a = scp.signal.butter(n, wn, 'bandpass', fs = fs)
@@ -224,17 +155,17 @@ def convert_bandpass(data, median = False):
         data[i][10] = bandpass(data[i], 10, bp = [1,40], sp = [0,50], gpass = 0.4, spass = 3, fs = sampling_rate, median = median)
         data[i][11] = bandpass(data[i], 11, bp = [1,40], sp = [0,50], gpass = 0.4, spass = 3, fs = sampling_rate, median = median)
     return data
-    
-def get_img_from_fig(fig, dpi=64):
-        buf = io.BytesIO()
-        fig.savefig(buf, format="png", dpi=dpi,bbox_inches=0)
-        buf.seek(0)
-        img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
-        buf.close()
-        img = cv2.imdecode(img_arr, 1)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = img[35:275,50:370]
-        return np.dot(img, [0.2989, 0.5870, 0.1140])
+
+# def get_img_from_fig(fig, dpi=64):
+#         buf = io.BytesIO()
+#         fig.savefig(buf, format="png", dpi=dpi,bbox_inches=0)
+#         buf.seek(0)
+#         img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
+#         buf.close()
+#         img = cv2.imdecode(img_arr, 1)
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         img = img[35:275,50:370]
+#         return np.dot(img, [0.2989, 0.5870, 0.1140])
 
 # def stft_(x,nperseg=40,size = (224,224),rgb=False):
         
